a9c5dd774daa34a91d77c13ca9cff074b17cc7f5
Rashi Gupta <rashiguptaim@gmail.com>
2024-04-07 12:18:06 +0530

    databases setup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log("connected");
+}).catch((e)=>{
+    console.log(e);
+})
 const app=express();
 
 app.listen(3000,()=>{
